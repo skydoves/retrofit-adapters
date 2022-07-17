673c1ed30e9441d9e204105560a67006cb46a64e
skydoves <skydoves@naver.com>
2022-07-17 22:48:11 +0900

    Add retrofit-adapters-serialization module

--- buildSrc/src/main/kotlin/com/skydoves/retrofit/adapters/Dependencies.kt
+++ buildSrc/src/main/kotlin/com/skydoves/retrofit/adapters/Dependencies.kt
@@ -5,6 +5,7 @@ object Versions {
   internal const val ANDROID_GRADLE_SPOTLESS = "6.7.0"
   internal const val GRADLE_NEXUS_PUBLISH_PLUGIN = "1.1.0"
   internal const val KOTLIN = "1.7.10"
+  internal const val KOTLIN_SERIALIZATION_JSON = "1.3.2"
   internal const val KOTLIN_GRADLE_DOKKA = "1.7.10"
   internal const val KOTLIN_BINARY_VALIDATOR = "0.11.0"
 
@@ -35,6 +36,9 @@ object Dependencies {
     "io.github.gradle-nexus:publish-plugin:${Versions.GRADLE_NEXUS_PUBLISH_PLUGIN}"
   const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.KOTLIN}"
   const val kotlinReflect = "org.jetbrains.kotlin:kotlin-reflect:${Versions.KOTLIN}"
+  const val kotlinSerializationPlugin = "org.jetbrains.kotlin:kotlin-serialization:${Versions.KOTLIN}"
+  const val kotlinSerializationJson =
+    "org.jetbrains.kotlinx:kotlinx-serialization-json:${Versions.KOTLIN_SERIALIZATION_JSON}"
   const val spotlessGradlePlugin =
     "com.diffplug.spotless:spotless-plugin-gradle:${Versions.ANDROID_GRADLE_SPOTLESS}"
   const val dokka = "org.jetbrains.dokka:dokka-gradle-plugin:${Versions.KOTLIN_GRADLE_DOKKA}"
