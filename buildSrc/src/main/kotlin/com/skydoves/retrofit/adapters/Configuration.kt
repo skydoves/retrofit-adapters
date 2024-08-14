43481e08dc0172e4cab9b8be2a16833f9b7c2e7f
skydoves <skydoves@naver.com>
2024-08-14 19:47:20 +0900

    Prepare for release 1.0.12

--- buildSrc/src/main/kotlin/com/skydoves/retrofit/adapters/Configuration.kt
+++ buildSrc/src/main/kotlin/com/skydoves/retrofit/adapters/Configuration.kt
@@ -23,9 +23,9 @@ object Configuration {
   const val minSdkDemo = 21
   const val majorVersion = 1
   const val minorVersion = 0
-  const val patchVersion = 11
+  const val patchVersion = 12
   const val versionName = "$majorVersion.$minorVersion.$patchVersion"
-  const val versionCode = 12
+  const val versionCode = 13
   const val snapshotVersionName = "$majorVersion.$minorVersion.${patchVersion + 1}-SNAPSHOT"
   const val artifactGroup = "com.github.skydoves"
 }
